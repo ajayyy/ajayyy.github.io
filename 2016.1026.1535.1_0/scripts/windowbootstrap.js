67dde7dfa22b103ef9b79fd607d65e1018bca09e
Ajay Ramachandran <ajayyy@users.noreply.github.com>
2016-11-03 20:33:14 -0400

    Add files via upload

---
+++ 2016.1026.1535.1_0/scripts/windowbootstrap.js
@@ -0,0 +1,17 @@
+"use strict";this.default_windowbootstrap=this.default_windowbootstrap||{};(function(_){var window=this;
+try{
+var aa,ba,ca,ea,da,ha,ia,ka,la;
+for(_.g,aa="function"==typeof Object.defineProperties?Object.defineProperty:function(a,b,c){if(c.get||c.set)throw new TypeError("ES3 does not support getters and setters.");a!=Array.prototype&&a!=Object.prototype&&(a[b]=c.value)},ba="undefined"!=typeof window&&window===this?this:"undefined"!=typeof window.global&&null!=window.global?window.global:this,ca=function(){ca=function(){};ba.Symbol||(ba.Symbol=da)},ea=0,da=function(a){return"jscomp_symbol_"+(a||"")+ea++},_.ga=function(){ca();var a=ba.Symbol.iterator;
+a||(a=ba.Symbol.iterator=ba.Symbol("iterator"));"function"!=typeof Array.prototype[a]&&aa(Array.prototype,a,{configurable:!0,writable:!0,value:function(){return _.fa(this)}});_.ga=function(){}},_.fa=function(a){var b=0;return ha(function(){return b<a.length?{done:!1,value:a[b++]}:{done:!0}})},ha=function(a){_.ga();a={next:a};a[ba.Symbol.iterator]=function(){return this};return a},ia=ba,ka=["String","prototype","repeat"],la=0;la<ka.length-1;la++){var ma=ka[la];ma in ia||(ia[ma]={});ia=ia[ma]}
+var na=ka[ka.length-1],oa=ia[na],pa=oa?oa:function(a){var b;if(null==this)throw new TypeError("The 'this' value for String.prototype.repeat must not be null or undefined");b=this+"";if(0>a||1342177279<a)throw new window.RangeError("Invalid count value");a|=0;for(var c="";a;)if(a&1&&(c+=b),a>>>=1)b+=b;return c};pa!=oa&&null!=pa&&aa(ia,na,{configurable:!0,writable:!0,value:pa});_.qa=_.qa||{};_.l=this;_.ra="closure_uid_"+(1E9*Math.random()>>>0);_.sa=Date.now||function(){return+new Date};
+
+}catch(e){_._DumpException(e)}
+try{
+_.Tg="https://support.google.com/chrome/answer/185277?hl="+window.chrome.i18n.getMessage("@@ui_locale");window.chrome.i18n.getMessage("@@ui_locale");_.Ug="https://support.google.com/hangouts/?p=chrome_desktop_app_outdated&hl="+window.chrome.i18n.getMessage("@@ui_locale");
+
+}catch(e){_._DumpException(e)}
+try{
+window.location.search&&window.addEventListener("load",function(){window.chrome.runtime.getBackgroundPage(function(a){a.__onExtWindowLoad(window)})});
+}catch(e){_._DumpException(e)}
+}).call(this,this.default_windowbootstrap);
+// Google Inc.
