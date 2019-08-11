b0069dee08fb5eaa00d80ab10f7ad2df6ca0a3ae
Ajay Ramachandran <dev@ajay.app>
2019-08-10 23:04:23 -0400

    Added stats for sponsor block

---
+++ getSponsorBlockStats.js
@@ -0,0 +1,55 @@
+function getTotalStats() {
+    sendRequestToCustomServer("GET", "https://sponsor.ajay.app/api/getTotalStats", function(xmlhttp) {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let response = JSON.parse(xmlhttp.responseText);
+
+            let totalUsers = response.userCount;
+            let totalSubmissions = response.totalSubmissions;
+            let minutesSaved = response.minutesSaved;
+
+            //start count ups
+            countUp(document.getElementById("totalUsers"), 0, totalUsers);
+            countUp(document.getElementById("totalSubmissions"), 0, totalSubmissions);
+            countUp(document.getElementById("hoursSaved"), 0, (minutesSaved / 60).toFixed(1));
+        }
+    });
+}
+
+function countUp(element, currentNumber, targetNumber) {
+    element.innerText = currentNumber;
+
+    if (currentNumber == targetNumber) {
+        //it's done
+        return;
+    }
+
+    //count up
+    let nextNumber = currentNumber + Math.ceil(targetNumber/100);
+    if (nextNumber >= targetNumber) {
+        nextNumber = targetNumber;
+    }
+
+    //call the next function
+    setTimeout(() => countUp(element, nextNumber, targetNumber), 1);
+}
+
+function sendRequestToCustomServer(type, fullAddress, callback) {
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open(type, fullAddress, true);
+
+    if (callback != undefined) {
+        xmlhttp.onreadystatechange = function () {
+        callback(xmlhttp, false);
+        };
+
+        xmlhttp.onerror = function(ev) {
+        callback(xmlhttp, true);
+        };
+    }
+
+    //submit this request
+    xmlhttp.send();
+}
+
+// getTotalStats();
