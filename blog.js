3ee85d4770146862684ba7f9b9ec52a0a459dafc
Ajay Ramachandran <dev@ajay.app>
2020-03-10 21:10:55 -0400

    Fixed missing bracket

--- blog.js
+++ blog.js
@@ -2,7 +2,7 @@ let hash = window.location.hash.substr(1);
 
 if (hash === "") {
     window.location.href("https://blog.ajay.app/");
-} else if (hash.startsWith("/tag") {
+} else if (hash.startsWith("/tag")) {
     window.location.href("https://blog.ajay.app/" + hash.split("=")[1]);
 } else {
     window.location.href("https://blog.ajay.app/" + hash);
