6763259fec4282190a903428baabb7aae38871f5
Ajay Ramachandran <dev@ajay.app>
2020-03-10 21:13:32 -0400

    Fixed redirect

--- blog.js
+++ blog.js
@@ -1,9 +1,9 @@
 let hash = window.location.hash.substr(1);
 
 if (hash === "") {
-    window.location.href("https://blog.ajay.app/");
+    window.location.href = "https://blog.ajay.app/";
 } else if (hash.startsWith("/tag")) {
-    window.location.href("https://blog.ajay.app/" + hash.split("=")[1]);
+    window.location.href = "https://blog.ajay.app/" + hash.split("=")[1];
 } else {
-    window.location.href("https://blog.ajay.app/" + hash);
+    window.location.href = "https://blog.ajay.app/" + hash;
 }
